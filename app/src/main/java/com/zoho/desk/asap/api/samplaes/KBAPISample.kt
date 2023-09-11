14f8537f415eaa2aa4e5c90648489aaa7071732d
prakash <prakash.vel@zohocorp.com>
2023-09-11 17:47:07 +0530

    Remaining Methods for KB APIs Added

--- app/src/main/java/com/zoho/desk/asap/api/samplaes/KBAPISample.kt
+++ app/src/main/java/com/zoho/desk/asap/api/samplaes/KBAPISample.kt
@@ -1,11 +1,25 @@
 package com.zoho.desk.asap.api.samplaes
 
+import com.zoho.desk.asap.api.ZDPortalCallback.ArticleCommentsCallback
+import com.zoho.desk.asap.api.ZDPortalCallback.ArticleDetailsCallback
+import com.zoho.desk.asap.api.ZDPortalCallback.ArticleFeedbackCallback
+import com.zoho.desk.asap.api.ZDPortalCallback.ArticleUpdateVoteCallback
+import com.zoho.desk.asap.api.ZDPortalCallback.ArticlesCallback
+import com.zoho.desk.asap.api.ZDPortalCallback.AttachmentsCallback
+import com.zoho.desk.asap.api.ZDPortalCallback.DownloadAttachmentCallback
 import com.zoho.desk.asap.api.ZDPortalCallback.KBCategoriesCallback
 import com.zoho.desk.asap.api.ZDPortalCallback.KBCategoryCallback
+import com.zoho.desk.asap.api.ZDPortalCallback.KBPrevNextArticlesCallback
 import com.zoho.desk.asap.api.ZDPortalException
 import com.zoho.desk.asap.api.ZDPortalKBAPI
+import com.zoho.desk.asap.api.response.ASAPAttachmentsList
+import com.zoho.desk.asap.api.response.ArticleCommentsList
+import com.zoho.desk.asap.api.response.KBArticle
+import com.zoho.desk.asap.api.response.KBArticlesList
 import com.zoho.desk.asap.api.response.KBCategoriesList
 import com.zoho.desk.asap.api.response.KBCategory
+import com.zoho.desk.asap.api.response.KBPrevNextArticlesList
+import java.io.InputStream
 
 class KBAPISample {
 
@@ -13,34 +27,265 @@ class KBAPISample {
         val params = hashMapOf<String, String>()
         params["from"] = "1"
         params["limit"] = "2"
+        params["locale"] = "localeCode"
+        params["sortBy"] = "name"
+//        params["sortBy"] = "order"
+        params["searchValue"] = "searchValue"
+        params["departmentId"] = "departmentId"
         params["hasArticles"] = "true"
+        params["include"] = "allArticleCount,sectionsCount"
         ZDPortalKBAPI.geRootKBCategories(object : KBCategoriesCallback {
-            override fun onException(exception: ZDPortalException?) {
-                //Root Categories Exception
-            }
-
-            override fun onKBCategoriesDownloaded(categoriesList: KBCategoriesList?) {
+            override fun onKBCategoriesDownloaded(kbCategoriesList: KBCategoriesList) {
                 //Categories data downloaded
             }
 
+            override fun onException(e: ZDPortalException) {
+                //Root Categories Exception
+            }
         }, params)
     }
 
     fun getKBRootCategoriesTree() {
-        val params = hashMapOf<String, String>()
-        params["from"] = "1"
-        params["limit"] = "2"
+        val params = HashMap<String, String>()
+        params["locale"] = "localeCode"
+        params["sortBy"] = "name"
+//        params["sortBy"] = "order"
+        params["hasArticles"] = "true"
+        params["include"] = "allArticleCount,sectionsCount"
         ZDPortalKBAPI.getKBCategoriesTree(object : KBCategoryCallback {
-            override fun onException(exception: ZDPortalException?) {
+            override fun onKBCategoryDownloaded(category: KBCategory) {
+                //Categories data downloaded
+            }
+
+            override fun onException(e: ZDPortalException) {
                 //Root Categories Exception
             }
-
-            override fun onKBCategoryDownloaded(p0: KBCategory?) {
-                //category data downloaded
-            }
-
         }, "RootCategoryId", params)
     }
 
+    fun getKBCategoryWithPermalink() {
+        val params = HashMap<String, String>()
+        params["permalink"] = "permalink"
+        params["locale"] = "localeCode"
+        params["departmentId"] = "departmentId"
+        params["hasArticles"] = "false"
+        params["include"] = "sectionsCount,allArticleCount,articlesCount"
+        ZDPortalKBAPI.getKBCategoryWithPermalink(object : KBCategoryCallback {
+            override fun onKBCategoryDownloaded(kbCategory: KBCategory) {
+                //categories data downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //categories Exception
+            }
+        }, params)
+    }
+
+    fun getArticles() {
+        val params = HashMap<String, String>()
+        params["categoryId"] = "categoryId"
+        params["from"] = "1"
+        params["limit"] = "50"
+        params["locale"] = "localeCode"
+        params["includeChildCategoryArticles"] = "true"
+        params["sortBy"] = "-likeCount"
+//        params["sortBy"] = "-createdTime"
+        params["tag"] = "TagName"
+        ZDPortalKBAPI.getArticlesList(object : ArticlesCallback {
+            override fun onArticlesDownloaded(kbArticlesList: KBArticlesList) {
+                //Articles data downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Articles Exception
+            }
+        }, params)
+    }
+
+    fun getArticleDetails() {
+        ZDPortalKBAPI.getArticleDetails(object : ArticleDetailsCallback {
+            override fun onArticleDetailsDownloaded(kbArticle: KBArticle) {
+                //Article details data downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article details Exception
+            }
+        }, "articleId", "localeCode", null)
+    }
+
+    fun getRelatedArticles() {
+        val params = HashMap<String, String>()
+        params["from"] = "1"
+        params["limit"] = "5"
+        params["locale"] = "localeCode"
+        ZDPortalKBAPI.getRelatedArticles(
+            "articleId",
+            "localeCode",
+            object : ArticlesCallback {
+                override fun onArticlesDownloaded(kbArticlesList: KBArticlesList) {
+                    //Related Articles data downloaded
+                }
+
+                override fun onException(exception: ZDPortalException) {
+                    //Related Articles Exception
+                }
+            },
+            params
+        )
+    }
+
+    fun getArticleAttachmentList() {
+        ZDPortalKBAPI.getArticleAttachments(object : AttachmentsCallback {
+            override fun onAttachmentsDownloaded(attachmentsList: ASAPAttachmentsList) {
+                //Article Attachments data downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article Attachments Exception
+            }
+        }, "articleId", "localeCode", null)
+    }
+
+    fun likeArticle() {
+        ZDPortalKBAPI.likeArticle(object : ArticleUpdateVoteCallback {
+            override fun onArticleUpdated() {
+                //Article Liked Successfully
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article Liked Exception
+            }
+        }, "articleId", "localeCode", null)
+    }
+
+    fun dislikeArticle() {
+        ZDPortalKBAPI.dislikeArticle(object : ArticleUpdateVoteCallback {
+            override fun onArticleUpdated() {
+                //Article Disliked Successfully
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article Dislike Exception
+            }
+        }, "articleId", "localeCode", null)
+    }
+
+    fun feedbackArticle() {
+        val feedbackContent = HashMap<String, String>()
+        feedbackContent["email"] = "userEmail"
+        feedbackContent["feedback"] = "feedbackContent"
+        ZDPortalKBAPI.articleFeedback(object : ArticleFeedbackCallback {
+            override fun onFeedbackPosted() {
+                //Article Feedback posted
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article Feedback Exception
+            }
+        }, "articleId", "localeCode", feedbackContent, null)
+    }
+
+    fun getArticleAttachmentContent() {
+        ZDPortalKBAPI.downloadArticleAttachment(object : DownloadAttachmentCallback {
+            override fun onAttachmentDownloaded(stream: InputStream) {
+                //Article Attachment content downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article Attachment Exception
+            }
+        }, "articleId", "localeCode", "attachmentId", null)
+    }
+
+    fun searchArticles() {
+        val params = HashMap<String, String>()
+        params["searchStr"] = "searchString"
+        params["from"] = "1"
+        params["limit"] = "50"
+        params["locale"] = "localeCode"
+        params["departmentId"] = "deptId"
+        params["categoryId"] = "categoryId"
+        ZDPortalKBAPI.searchArticles(object : ArticlesCallback {
+            override fun onArticlesDownloaded(kbArticlesList: KBArticlesList) {
+                //Categories data downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Root Categories Exception
+            }
+        }, false, params)
+    }
+
+    fun getArticleDetailsByPermalink() {
+        val params = HashMap<String, String>()
+        params["permalink"] = "permalink"
+        ZDPortalKBAPI.getArticleDetailsWithPermalink(object : ArticleDetailsCallback {
+            override fun onArticleDetailsDownloaded(kbArticle: KBArticle) {
+                //Article data downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article Exception
+            }
+        }, params)
+    }
+
+    fun getArticleComments() {
+        val params = HashMap<String, String>()
+        params["from"] = "1"
+        params["limit"] = "5"
+        params["sortBy"] = "-commentedTime"
+        ZDPortalKBAPI.getArticleComments(object : ArticleCommentsCallback {
+            override fun onCommentsDownloaded(commentsList: ArticleCommentsList) {
+                //Article comments data downloaded
+            }
+
+            override fun onException(exception: ZDPortalException) {
+                //Article comments Exception
+            }
+        }, "articleId", "localeCode", params)
+    }
+
+    fun getPreviousNextArticles() {
+        val params = HashMap<String, String>()
+        params["from"] = "1"
+        params["limit"] = "5"
+        ZDPortalKBAPI.getPrevNextArticles(
+            "articleId",
+            "localeCode",
+            object : KBPrevNextArticlesCallback {
+                override fun onArticlesDownloaded(kbArticlesList: KBPrevNextArticlesList) {
+                    //Previous Next Articles data downloaded
+                }
+
+                override fun onException(exception: ZDPortalException) {
+                    //Previous Next Articles Exception
+                }
+            },
+            params
+        )
+    }
+
+    fun getArticlesByTag() {
+        val params = HashMap<String, String>()
+        params["categoryId"] = "categoryId"
+        params["tag"] = "tag name"
+        params["from"] = "1"
+        params["limit"] = "5"
+        params["locale"] = "localeCode"
+        ZDPortalKBAPI.articlesSearchByTag(
+            object : ArticlesCallback {
+                override fun onArticlesDownloaded(kbArticlesList: KBArticlesList) {
+                    //Articles data downloaded
+                }
+
+                override fun onException(exception: ZDPortalException) {
+                    //Articles Exception
+                }
+            },
+            params
+        )
+    }
 
 }
--- app/src/main/java/com/zoho/desk/asap/api/samplaes/KBAPISampleJava.java
+++ app/src/main/java/com/zoho/desk/asap/api/samplaes/KBAPISampleJava.java
@@ -1,12 +1,17 @@
 package com.zoho.desk.asap.api.samplaes;
 
-import com.zoho.desk.asap.api.ZDPortalAPI;
 import com.zoho.desk.asap.api.ZDPortalCallback;
 import com.zoho.desk.asap.api.ZDPortalException;
 import com.zoho.desk.asap.api.ZDPortalKBAPI;
+import com.zoho.desk.asap.api.response.ASAPAttachmentsList;
+import com.zoho.desk.asap.api.response.ArticleCommentsList;
+import com.zoho.desk.asap.api.response.KBArticle;
+import com.zoho.desk.asap.api.response.KBArticlesList;
 import com.zoho.desk.asap.api.response.KBCategoriesList;
 import com.zoho.desk.asap.api.response.KBCategory;
+import com.zoho.desk.asap.api.response.KBPrevNextArticlesList;
 
+import java.io.InputStream;
 import java.util.HashMap;
 
 public class KBAPISampleJava {
@@ -15,37 +20,297 @@ public class KBAPISampleJava {
         HashMap<String, String> params = new HashMap<>();
         params.put("from", "1");
         params.put("limit", "2");
+        params.put("locale", "localeCode");
+        params.put("sortBy", "name");
+//        params.put("sortBy", "order");
+        params.put("searchValue", "searchValue");
+        params.put("departmentId", "departmentId");
         params.put("hasArticles", "true");
+        params.put("include", "allArticleCount,sectionsCount");
         ZDPortalKBAPI.geRootKBCategories(new ZDPortalCallback.KBCategoriesCallback() {
             @Override
             public void onKBCategoriesDownloaded(KBCategoriesList kbCategoriesList) {
-                //Root Categories Exception
+                //Categories data downloaded
             }
 
             @Override
             public void onException(ZDPortalException e) {
-                //Categories data downloaded
+                //Root Categories Exception
             }
         }, params);
     }
 
     public void getKBRootCategoriesTree() {
         HashMap<String, String> params = new HashMap<>();
-        params.put("from", "1");
-        params.put("limit", "2");
+        params.put("locale", "localeCode");
+        params.put("sortBy", "name");
+//        params.put("sortBy", "order");
         params.put("hasArticles", "true");
+        params.put("include", "allArticleCount,sectionsCount");
         ZDPortalKBAPI.getKBCategoriesTree(new ZDPortalCallback.KBCategoryCallback() {
             @Override
             public void onKBCategoryDownloaded(KBCategory category) {
-                //Root Categories Exception
+                //Categories data downloaded
             }
 
             @Override
             public void onException(ZDPortalException e) {
-                //Categories data downloaded
+                //Root Categories Exception
             }
         },"RootCategoryId", params);
     }
 
+    public void getKBCategoryWithPermalink(){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("permalink", "permalink");
+        params.put("locale", "localeCode");
+        params.put("departmentId", "departmentId");
+        params.put("hasArticles", "false");
+        params.put("include", "sectionsCount,allArticleCount,articlesCount");
+        ZDPortalKBAPI.getKBCategoryWithPermalink(new ZDPortalCallback.KBCategoryCallback(){
+
+            @Override
+            public void onKBCategoryDownloaded(KBCategory kbCategory) {
+                //categories data downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //categories Exception
+            }
+        }, params);
+    }
+
+    public void getArticles() {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("categoryId", "categoryId");
+        params.put("from", "1");
+        params.put("limit", "50");
+        params.put("locale", "localeCode");
+        params.put("includeChildCategoryArticles", "true");
+        params.put("sortBy", "-likeCount");
+//        params.put("sortBy", "-createdTime");
+        params.put("tag", "TagName");
+        ZDPortalKBAPI.getArticlesList(new ZDPortalCallback.ArticlesCallback() {
+            @Override
+            public void onArticlesDownloaded(KBArticlesList kbArticlesList) {
+                //Articles data downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Articles Exception
+            }
+        }, params);
+    }
+
+    public void getArticleDetails() {
+        ZDPortalKBAPI.getArticleDetails(new ZDPortalCallback.ArticleDetailsCallback() {
+            @Override
+            public void onArticleDetailsDownloaded(KBArticle kbArticle) {
+                //Article details data downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article details Exception
+            }
+        }, "articleId", "localeCode", null);
+    }
+
+    public void getRelatedArticles(){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("from", "1");
+        params.put("limit", "5");
+        params.put("locale", "localeCode");
+        ZDPortalKBAPI.getRelatedArticles(
+            "articleId",
+            "localeCode",
+            new ZDPortalCallback.ArticlesCallback() {
+                @Override
+                public void onArticlesDownloaded(KBArticlesList kbArticlesList) {
+                    //Related Articles data downloaded
+                }
+                @Override
+                public void onException(ZDPortalException exception) {
+                    //Related Articles Exception
+                }
+            },
+            params
+        );
+    }
+
+    public void getArticleAttachmentList(){
+        ZDPortalKBAPI.getArticleAttachments(new ZDPortalCallback.AttachmentsCallback(){
+            @Override
+            public void onAttachmentsDownloaded(ASAPAttachmentsList attachmentsList) {
+                //Article Attachments data downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article Attachments Exception
+            }
+        }, "articleId", "localeCode", null);
+    }
+
+    public void likeArticle(){
+        ZDPortalKBAPI.likeArticle(new ZDPortalCallback.ArticleUpdateVoteCallback() {
+            @Override
+            public void onArticleUpdated() {
+                //Article Liked Successfully
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article Liked Exception
+            }
+        }, "articleId", "localeCode", null);
+    }
+
+    public void dislikeArticle(){
+        ZDPortalKBAPI.dislikeArticle(new ZDPortalCallback.ArticleUpdateVoteCallback() {
+            @Override
+            public void onArticleUpdated() {
+                //Article Disliked Successfully
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article Dislike Exception
+            }
+        }, "articleId", "localeCode", null);
+    }
+
+    public void feedbackArticle(){
+        HashMap<String, String> feedbackContent = new HashMap<>();
+        feedbackContent.put("email", "userEmail");
+        feedbackContent.put("feedback", "feedbackContent");
+        ZDPortalKBAPI.articleFeedback(new ZDPortalCallback.ArticleFeedbackCallback() {
+            @Override
+            public void onFeedbackPosted() {
+                //Article Feedback posted
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article Feedback Exception
+            }
+        }, "articleId", "localeCode", feedbackContent, null);
+    }
+
+    public void getArticleAttachmentContent(){
+        ZDPortalKBAPI.downloadArticleAttachment(new ZDPortalCallback.DownloadAttachmentCallback(){
+            @Override
+            public void onAttachmentDownloaded(InputStream stream) {
+                //Article Attachment content downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article Attachment Exception
+            }
+        }, "articleId", "localeCode", "attachmentId", null);
+    }
+
+    public void searchArticles() {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("searchStr", "searchString");
+        params.put("from", "1");
+        params.put("limit", "50");
+        params.put("locale", "localeCode");
+        params.put("departmentId", "deptId");
+        params.put("categoryId", "categoryId");
+        ZDPortalKBAPI.searchArticles(new ZDPortalCallback.ArticlesCallback() {
+            @Override
+            public void onArticlesDownloaded(KBArticlesList kbArticlesList) {
+                //Categories data downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Root Categories Exception
+            }
+        }, false, params);
+    }
+
+    public void getArticleDetailsByPermalink(){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("permalink", "permalink");
+        ZDPortalKBAPI.getArticleDetailsWithPermalink(new ZDPortalCallback.ArticleDetailsCallback() {
+            @Override
+            public void onArticleDetailsDownloaded(KBArticle kbArticle) {
+                //Article data downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article Exception
+            }
+        }, params);
+    }
+
+    public void getArticleComments(){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("from", "1");
+        params.put("limit", "5");
+        params.put("sortBy", "-commentedTime");
+        ZDPortalKBAPI.getArticleComments(new ZDPortalCallback.ArticleCommentsCallback() {
+            @Override
+            public void onCommentsDownloaded(ArticleCommentsList commentsList) {
+                //Article comments data downloaded
+            }
+
+            @Override
+            public void onException(ZDPortalException exception) {
+                //Article comments Exception
+            }
+        }, "articleId", "localeCode", params);
+    }
+
+    public void getPreviousNextArticles(){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("from", "1");
+        params.put("limit", "5");
+        ZDPortalKBAPI.getPrevNextArticles(
+            "articleId",
+            "localeCode",
+            new ZDPortalCallback.KBPrevNextArticlesCallback() {
+                @Override
+                public void onArticlesDownloaded(KBPrevNextArticlesList kbArticlesList) {
+                    //Previous Next Articles data downloaded
+                }
+
+                @Override
+                public void onException(ZDPortalException exception) {
+                    //Previous Next Articles Exception
+                }
+            },
+            params
+        );
+    }
+
+    public void getArticlesByTag(){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("categoryId", "categoryId");
+        params.put("tag", "tag");
+        params.put("from", "1");
+        params.put("limit", "5");
+        params.put("locale", "localeCode");
+        ZDPortalKBAPI.articlesSearchByTag(
+            new ZDPortalCallback.ArticlesCallback() {
+                @Override
+                public void onArticlesDownloaded(KBArticlesList kbArticlesList) {
+                    //Articles data downloaded
+                }
+
+                @Override
+                public void onException(ZDPortalException exception) {
+                    //Articles Exception
+                }
+            },
+            params
+        );
+    }
 
 }
